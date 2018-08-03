2f0223397f0a9a7c13d427fc9b77458b1d7faf96
Joey <joey.xf@gmail.com>
2018-08-03 10:43:33 +0800

    把路由以及日志模块引入
    go风格的框架基本完成

--- main.php
+++ main.php
@@ -8,7 +8,18 @@ use Amp\Http\Server\Request;
 use Amp\Http\Server\Response;
 use Amp\Http\Status;
 use Amp\Socket;
-use Psr\Log\NullLogger;
+// use Psr\Log\NullLogger;
+
+// route part
+use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\Routing\RequestContext;
+use Symfony\Component\Routing\RouteCollection;
+use Symfony\Component\Routing\Route;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
+
+// log part
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
 
 // Run this script, then visit http://localhost:1337/ in your browser.
 
@@ -17,12 +28,24 @@ Amp\Loop::run(function () {
         Socket\listen("0.0.0.0:1337"),
         Socket\listen("[::]:1337"),
     ];
+
+    $logger = new Logger('app');
+    $logger->pushHandler(new StreamHandler('app.log', Logger::DEBUG));
+
+    $route = new Route('/foo', array('handler' => 'fooHandler'));
+    $routes = new RouteCollection();
+    $routes->add('route_name', $route);
+    $context = new RequestContext('/');
+    $matcher = new UrlMatcher($routes, $context);
     
-    $server = new Server($sockets, new CallableRequestHandler(function (Request $request) {
-        return new Response(Status::OK, [
-            "content-type" => "text/plain; charset=utf-8"
-        ], "Hello, World!");
-    }), new NullLogger);
+    $server = new Server($sockets, new CallableRequestHandler(function (Request $request) use ($matcher, $logger) {
+        try {
+            $params = $matcher->match($request->getUri()->getPath());
+            return $params['handler']($request);
+        } catch (ResourceNotFoundException $e) {
+            return new Response(404, ['content-type" => "text/plain; charset=utf-8'], '404 not found');
+        }
+    }), $logger);
 
     yield $server->start();
 
@@ -33,3 +56,7 @@ Amp\Loop::run(function () {
         yield $server->stop();
     });
 });
+
+function fooHandler(Request $request) {
+    return new Response(200, ['content-type" => "text/plain; charset=utf-8'], 'foo handler');
+}
